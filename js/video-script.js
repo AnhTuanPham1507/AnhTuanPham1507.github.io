4fae68a6f6253e27fa16aff1566436ae6bd38e45
AnhTuanPham1507 <62019976+AnhTuanPham1507@users.noreply.github.com>
2021-03-25 14:58:57 +0700

    Update video-script.js

--- js/video-script.js
+++ js/video-script.js
@@ -6,8 +6,7 @@ const peer = new Peer({
     secure:true, 
     port:443,
     config: {'iceServers': [
-        { url: 'stun:stun.l.google.com:19302' },
-        { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
+        { url: 'stun:stun.l.google.com:19302' }
    ]}
 })
 const videoGrid = document.getElementById('video-grid')
